40ec71946f0fd2530c0c0df60715b3f6b3720972
Richa-devaliya <richadevaliya01@gmail.com>
2025-04-27 02:12:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request,jsonify
+from chatbot import get_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/predict",methods=["POST"])
+def predict():
+	key = request.get_json().get("api-key")
+	text = request.get_json().get("message")
+	if key == '0000000000':
+		response = get_response(text)
+		message = {"answer": response}
+		return jsonify(message)
+
+	else:
+		return jsonify(
+			{
+			"error": "invalid api key"
+			}
+		)
+
+if __name__=='__main__':
+	app.run(debug=True)
